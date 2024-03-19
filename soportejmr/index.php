9804db672b2bb7478dacdba4521c3ef898ceab61
LuisMonroy123 <irivapala52@gmail.com>
2024-03-19 16:39:15 -0600

    cambios de cock a lalo

--- soportejmr/index.php
+++ soportejmr/index.php
@@ -398,9 +398,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cock </title>
+    <title>Tasi</title>
 </head>
 <body>
-    <h1>COCK</h1>
+    <h1>Tasi me cae bien</h1>
 </body>
 </html>
